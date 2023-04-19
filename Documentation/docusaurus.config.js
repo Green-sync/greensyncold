d381ca687d7ae4b8692a2960a3c6271e291e88fc
Mitch Chimwemwe Chanza <mitch.chanza@gmail.com>
2023-04-20 01:56:01 +0200

    feat: chnage base url

--- Documentation/docusaurus.config.js
+++ Documentation/docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: "https://docs.greensync.co.za",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/greensync/",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.

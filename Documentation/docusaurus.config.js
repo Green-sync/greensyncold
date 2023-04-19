cec25c78610bb613b4dcd568a11da33527f5c6bf
Mitch Chimwemwe Chanza <mitch.chanza@gmail.com>
2023-04-20 01:22:59 +0200

    feat: chnage  base url

--- Documentation/docusaurus.config.js
+++ Documentation/docusaurus.config.js
@@ -11,10 +11,10 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://Green-sync.github.io",
+  url: "https://docs.greensync.co.za",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/greensync",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.

82da3e80fe499250ef2ecb83c90d78a52081f8f0
MathibelaD <dineomathibela161@gmail.com>
2023-04-24 15:09:04 +0200

    navigate to dashboard

--- Applications/client/src/App.tsx
+++ Applications/client/src/App.tsx
@@ -4,12 +4,13 @@ import LoginPage from "./pages/loginPage"
 import DashboardPage from "./pages/dashboard"
 import {env} from "./utils"
 import LoginComponent from "./components/authentication/loginComponent"
+import LoginForms from "./components/forms/LoginForms"
 function App() {
 console.log(env)
   return (
     <>
 <Routes>
-  <Route path="/signIn" element={<LoginComponent />} />
+  <Route path="/signIn" element={<LoginForms />} />
   <Route path="/dashboard" element={<DashboardPage/>}/>
   <Route path="*" element={<ErrorPage/>}/>
 </Routes>

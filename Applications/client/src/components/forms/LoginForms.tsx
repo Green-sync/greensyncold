dae310bc4c5cddeccd270d76eb8991dcfab41b73
MathibelaD <dineomathibela161@gmail.com>
2023-04-24 16:45:16 +0200

    Co-authored-by: Mitch Chimwemwe Chanza <mitch1009@users.noreply.github.com>

--- Applications/client/src/components/forms/LoginForms.tsx
+++ Applications/client/src/components/forms/LoginForms.tsx
@@ -1,7 +1,7 @@
 
 import { useState } from "react";
-import {LoginnWithEmailAndPassword} from "../../firebase"
-import { CreateUserAccount, SocialLogin, UserDetails } from "../../firebase";
+import { CreateUserAccount, SocialLogin, LoginnWithEmailAndPassword,UserDetails } from "../../firebase";
+import { useNavigate } from "react-router-dom";
 
 export default function LoginForms() {
     const [signInType, setSignInType] = useState(false)
@@ -31,6 +31,15 @@ export default function LoginForms() {
         userData["farmType"] = e.target.value;
         setUserData({...userData}); 
       };
+    const authenticate = async (e: any) => {
+      e.preventDefault()
+      const [option,provider] = e.target.id.split(',')
+      if(option ==="social"){
+        const isloggedIn=  await SocialLogin(provider)
+        return isloggedIn ? nav("/"): null
+      }
+
+    }
 
     return (
       <>
@@ -220,7 +229,8 @@ export default function LoginForms() {
   
                 <div className="mt-6 grid grid-cols-2 gap-4">
                   <a
-                    href="#"
+                    href="#" id="social,google"
+                    onClick={authenticate}
                     className="flex w-full items-center justify-center gap-3 rounded-md bg-[#1D9BF0] px-3 py-1.5 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#1D9BF0]"
                   >
                     <svg height="32px" id="Layer_1" version="1.1" viewBox="0 0 56.6934 56.6934" width="32px" xmlns="http://www.w3.org/2000/svg" ><path d="M51.981,24.4812c-7.7173-0.0038-15.4346-0.0019-23.1518-0.001c0.001,3.2009-0.0038,6.4018,0.0019,9.6017  c4.4693-0.001,8.9386-0.0019,13.407,0c-0.5179,3.0673-2.3408,5.8723-4.9258,7.5991c-1.625,1.0926-3.492,1.8018-5.4168,2.139  c-1.9372,0.3306-3.9389,0.3729-5.8713-0.0183c-1.9651-0.3921-3.8409-1.2108-5.4773-2.3649  c-2.6166-1.8383-4.6135-4.5279-5.6388-7.5549c-1.0484-3.0788-1.0561-6.5046,0.0048-9.5805  c0.7361-2.1679,1.9613-4.1705,3.5708-5.8002c1.9853-2.0324,4.5664-3.4853,7.3473-4.0811c2.3812-0.5083,4.8921-0.4113,7.2234,0.294  c1.9815,0.6016,3.8082,1.6874,5.3044,3.1163c1.5125-1.5039,3.0173-3.0164,4.527-4.5231c0.7918-0.811,1.624-1.5865,2.3908-2.4196  c-2.2928-2.1218-4.9805-3.8274-7.9172-4.9056C32.0723,4.0363,26.1097,3.995,20.7871,5.8372  C14.7889,7.8907,9.6815,12.3763,6.8497,18.0459c-0.9859,1.9536-1.7057,4.0388-2.1381,6.1836  C3.6238,29.5732,4.382,35.2707,6.8468,40.1378c1.6019,3.1768,3.8985,6.001,6.6843,8.215c2.6282,2.0958,5.6916,3.6439,8.9396,4.5078  c4.0984,1.0993,8.461,1.0743,12.5864,0.1355c3.7284-0.8581,7.256-2.6397,10.0725-5.24c2.977-2.7358,5.1006-6.3403,6.2249-10.2138  C52.5807,33.3171,52.7498,28.8064,51.981,24.4812z"/>
@@ -229,12 +239,13 @@ export default function LoginForms() {
                   </a>
   
                   <a
-                    href="#"
+                    href="#" id="social,facebook"
+                    onClick={authenticate}
                     className="flex w-full items-center justify-center gap-3 rounded-md bg-[#24292F] px-3 py-1.5 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#24292F]"
                   >
                     <svg height="32px" id="Layer_1" version="1.1" viewBox="0 0 56.693 56.693" width="32px"  xmlns="http://www.w3.org/2000/svg" ><path d="M28.347,5.157c-13.6,0-24.625,11.027-24.625,24.625c0,13.6,11.025,24.623,24.625,24.623c13.6,0,24.625-11.023,24.625-24.623  C52.972,16.184,41.946,5.157,28.347,5.157z M34.864,29.679h-4.264c0,6.814,0,15.207,0,15.207h-6.32c0,0,0-8.307,0-15.207h-3.006  V24.31h3.006v-3.479c0-2.49,1.182-6.377,6.379-6.377l4.68,0.018v5.215c0,0-2.846,0-3.398,0c-0.555,0-1.34,0.277-1.34,1.461v3.163  h4.818L34.864,29.679z"/>
                     </svg>
-                    <span className="text-sm font-semibold leading-6">GitHub</span>
+                    <span className="text-sm font-semibold leading-6">Facebook</span>
                   </a>
                 </div>
               </div>

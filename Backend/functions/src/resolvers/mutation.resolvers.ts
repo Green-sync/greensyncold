883800e4ecff668254a86e355cb81614ee63ca18
Daniel Mokoena <danielmduduzi88@gmail.com>
2023-04-24 10:33:15 +0200

    fix: farmMutationResolver fixed

--- Backend/functions/src/resolvers/mutation.resolvers.ts
+++ Backend/functions/src/resolvers/mutation.resolvers.ts
@@ -1,8 +1,10 @@
 import { CropsMutationResolver} from "../modules"
 import { StockMutationResolver } from "../modules"
+import { FarmMutationResolver } from "../modules/farmer/farm"
 export const MutationResolver = {
     ...CropsMutationResolver,
-    ...StockMutationResolver
+    ...StockMutationResolver,
+    ...FarmMutationResolver
 }
 
 

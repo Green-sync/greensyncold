4651cf5af12cb0f2d2b9e87cf20975126ba707cc
Mitch Chimwemwe Chanza <hello@mitch.guru>
2023-04-25 00:04:40 +0200

    feat: initialize tailwindcss closes #35

---
+++ Packages/ui/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["src/**/*.{html,css,scss,js,jsx,ts,tsx,md",
+"*.{html}"
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+

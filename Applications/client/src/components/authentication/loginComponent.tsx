050728591e85e75feb1f2d1bcf686c03a35f4692
Dzuni-Cuthberts <mongwed@gmail.com>
2023-04-24 11:37:47 +0200

    dashboard

---
+++ Applications/client/src/components/authentication/loginComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const loginComponent = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default loginComponent
---
+++ Applications/client/src/components/dashboard/farm/liveStock.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const liveStock = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default liveStock

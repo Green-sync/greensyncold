4e8befb7725dbd0b067156936a7b9884dd8b368e
Dzuni-Cuthberts <mongwed@gmail.com>
2023-04-24 18:38:17 +0200

    dashboard-green

--- Applications/client/src/App.tsx
+++ Applications/client/src/App.tsx
@@ -1,9 +1,11 @@
 import { Routes, Route} from "react-router-dom"
 import ErrorPage from "./pages/errorPage"
 import LoginPage from "./pages/loginPage"
-import DashboardPage from "./pages/dashboard"
 import { SignUp } from "./pages/signUpPage"
 import {env} from "./utils"
+import DashboardComponent from "./pages/dashboard"
+import './style/agriDashboard.css'
+
 function App() {
 console.log(env)
   return (
@@ -11,7 +13,7 @@ console.log(env)
 <Routes>
   <Route index path="/" element={<LoginPage/>}/>
   <Route path="/signUp" element={<SignUp />} />
-  <Route path="/dashboard" element={<DashboardPage/>}/>
+  <Route path="/dashboard" element={<DashboardComponent/>}/>
   <Route path="*" element={<ErrorPage/>}/>
 </Routes>
     </>

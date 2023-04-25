f4d78aab1c3ffc774b7e61787b1ba3f84bb6d5a2
M9china <nqobilencaza2@gmail.com>
2023-04-25 17:11:55 +0200

    query: weather data

--- Backend/functions/src/modules/farmer/weather/WeatherService.ts
+++ Backend/functions/src/modules/farmer/weather/WeatherService.ts
@@ -2,18 +2,68 @@ import { db } from "../../../utils";
 
 export class WeatherService {
 
-    static async addWeather(Weather: any) {
-        try {
-          
-            await db.collection("WeatherData").add(Weather)
-          return {
-            message: "Weather details successfully added", success: true,
-          };
-        } catch (error) {
-          return {
-            message: `${error} failed to add Weather details`, success: false,
-          };
-        }
+  static async addWeather(Weather: any) {
+      try {
+        
+          await db.collection("WeatherData").add(Weather)
+        return {
+          message: "Weather details successfully added", success: true,
+        
+        };
+      } catch (error) {
+        return {
+          message: `${error} failed to add Weather details`, success: false,
+        };
       }
+    }
+
+  static async getAllWeathersData(user: any) {
+      try {
+        const WeatherDataArray: any = [];
+        const WD = await db.collection("WeatherData").get()
+        // Loop through the documents in the IotDevice collection and push their data into the IotDeviceArray
+        WD.forEach((doc) => {
+          WeatherDataArray.push({ id: doc.id, ...doc.data() });
+        });
+        return WeatherDataArray
+    } catch (error) {
+        return {
+            message: `${error} failed to retrieve Iotdevices`, success: false,
+        };
+    }
+  }  
 
 }
+
+/*export class WeatherService {
+  static addWeather(weather: any) {
+      throw new Error("Method not implemented.");
+  }
+  static async getWeatherData() {
+    try {
+      const querySnapshot = await db.collection("WeatherData").get();
+      const weatherData: any[] = [];
+      querySnapshot.forEach((doc: any) => {
+        weatherData.push({
+          id: doc.id,
+          ...doc.data(),
+        });
+      });
+      return {
+        message: "Weather data retrieved successfully",
+        success: true,
+        data: weatherData,
+      };
+    } catch (error) {
+      return {
+        message: `${error} failed to retrieve weather data`,
+        success: false,
+        data: null,
+      };
+    }
+  }
+}
+export {db}*/
+
+
+
--- Backend/functions/src/modules/farmer/weather/schema/query.resolvers.ts
+++ Backend/functions/src/modules/farmer/weather/schema/query.resolvers.ts
@@ -1,5 +1,21 @@
-export const WeatherQueryResolver = {
+/*export const WeatherQueryResolver = {
     getWeatherData: () => { 
-        return {message: "", success:true}
+        return {WeatherData: "", success:true}
     },
+
+    import { IotDeviceService } from "../IotDeviceService"
+
+export const IotDeviceQueryResolver = {
+    getAllIoTDevice: (_:unknown, __:unknown, context: {user:any, db: any}) => { 
+      return IotDeviceService.getAllIoTDevices(context.user)
+    }
+}*/
+
+import { WeatherService } from "../WeatherService"
+
+export const WeatherQueryResolver = {
+    getAllWeathersData: (_:unknown, __:unknown, context: {user:any, db: any}) => { 
+        return WeatherService.getAllWeathersData(context.user)
+    }
 }
+

8b91bc297f90441cf11010d2c8e92aeb77f5d40c
Mitch Chimwemwe Chanza <hello@mitch.guru>
2023-04-25 08:53:45 +0200

    feat: add auth story

---
+++ Packages/ui/src/stories/Auth.stories.ts
@@ -0,0 +1,20 @@
+import { AuthComponent } from "../components";
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+    component: AuthComponent,
+    title: "Components/Auth",
+    args: {
+        option: "login", theme: "default"
+    }
+}  satisfies Meta<typeof AuthComponent>;
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Login: Story = {
+    args: {
+        theme: "scotchy",
+        image: "/login.jpeg",
+        option: "login"
+    }
+}
